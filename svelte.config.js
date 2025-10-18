880efcc14f70c91d8cfefb035193594455209305
lostmusician <ivan_chiew@yahoo.com.sg>
2025-10-18 11:25:15 +0800

    hopefully made it compatible with vercel

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,11 @@ import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: { 
+		adapter: adapter({
+			runtime: 'nodejs24.x'
+		})
+	},
 	preprocess: [mdsvex()],
 	extensions: ['.svelte', '.svx']
 };
